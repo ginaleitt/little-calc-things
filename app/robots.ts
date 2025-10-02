af7f0302f2412a1b49e5e6c1e437713e82363f45
ginaleitt <ginaleitterman@gmail.com>
2025-10-02 08:20:30 -0700

    update robot file

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://little-calc-things.vercel.app/sitemap.xml' // Update with your actual domain
+  const baseUrl = 'https://little-calc-things.vercel.app/' // Update with your actual domain
 
   return {
     rules: {
