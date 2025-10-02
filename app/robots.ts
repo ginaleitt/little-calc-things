ba8f09a4d37932e6721ff225b9af412ebf675444
ginaleitt <ginaleitterman@gmail.com>
2025-10-02 08:13:13 -0700

    update robot file

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://ginalworks.com' // Update with your actual domain
+  const baseUrl = 'https://little-calc-things.com' // Update with your actual domain
 
   return {
     rules: {
